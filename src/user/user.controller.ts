7ccf5da75ced1130c628ff5dd423088ac564e1ec
nekcyag <contatodoneli@gmail.com>
2025-01-29 14:53:21 -0300

    feat: Create sign Post route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+    @Post('signup')
+    async signupUser(
+      @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel> {
+      return this.userService.createUser(userData);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    @Inject()
+    private readonly prisma: PrismaService;
+
+
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        const hashedPassword = await bcrypt.hash(data.password, 10); // criptografia da senha
+        return this.prisma.user.create({ data: { ...data, password: hashedPassword } });
+      }
+    
+      async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+      }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({
+          data,
+          where,
+        });
+      }
+    
+      async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({
+          where,
+        });
+      }
+}
